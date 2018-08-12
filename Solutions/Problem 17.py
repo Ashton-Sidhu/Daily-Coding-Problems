6bf1391cc23c4d24c7f44a189046ce53f187e3c1
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2018-08-12 01:58:02 -0400

    init

--- Solutions/Problem 17.py
+++ Solutions/Problem 17.py
@@ -23,8 +23,15 @@
 
 #SOLUTION
 
+#First step is to split the string by \n to separate items and then count the number of \t to determine the level of the directory or file.
+#If the current tab count is greate than the previous you are one level lower so set the length of the directory up to this point to subLength and then add the item to the count
+#If there is a period in the item, then it is a file so check if the total length to this point is greater than the previous total length and then set the total length.
+#Then set the count to the directory length before you added the length of the file (variable: subLength)
+#If the item is on the same level as the previous item, set the count to length of one directory higher (variable: sublength) and then add the length of the item
+#Finally, set the directory level of the current item.
 
 #Time Complexity: O(N) iterating through the split string list once.
+#Space Complexity: O(X) where x is the number of total items in a directory.
 
 
 def LongestPath(s):
@@ -33,10 +40,10 @@ def LongestPath(s):
 
     init = len(pathList[0])
     subLength = init
-    count = init
     tabCount = 0
     total = 0
-
+    count = 0
+    
     for item in pathList:
 
         tempTabCount = item.count('\t')
@@ -44,15 +51,13 @@ def LongestPath(s):
         if tempTabCount == 1:
             count = init
             subLength = init
-            tabCount = 0
+            tabCount = 1
 
         if tempTabCount > tabCount:
             subLength = count
-            count += len(item.strip()) + 1            
-            tabCount = tempTabCount
-
-        if '.' in item:
-            count += len(item.strip()) + 1
+            count += len(item.strip()) + 1       
+           
+        if '.' in item:           
             if count > total:
                 total = count
             count = subLength
@@ -60,11 +65,15 @@ def LongestPath(s):
         if tempTabCount == tabCount:
             count = subLength
             count += len(item.strip()) + 1
+        
+        tabCount = tempTabCount
 
     return total
 
 def main():
-    print(LongestPath('dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext'))
+    assert(LongestPath('dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext') == 20)
+    assert(LongestPath('dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext') == 32)
+    assert(LongestPath('dir\n\tsubdir1\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2') == 0)
 
 if __name__ == '__main__':
     main()
